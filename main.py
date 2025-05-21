95def76c5e0024bd81794580b671eb38e131d0e6
unknown <cjsgnsvlfcjs@gmail.com>
2025-05-21 21:14:37 +0900

    필요없는 html파일 삭제

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+
 from plenary_bills_list import router as plenary_router
+from plenary_bills_detail import router as plenary_detail_router
+from legislation_notice_ongoing_list import router as notice_list_router
+#from legislation_notice_ongoing_detail import router as notice_detail_router
 
 app = FastAPI()
+
 app.include_router(plenary_router)
+app.include_router(plenary_detail_router)
+app.include_router(notice_list_router)
+#app.include_router(notice_detail_router)
