6c58fba023795c3d462321cab8f355b0760490cb
unknown <cjsgnsvlfcjs@gmail.com>
2025-05-21 22:27:11 +0900

    main.py : 라우팅

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+from plenary_bills_list import router as plenary_router
+from plenary_bills_detail import router as plenary_detail_router
+from legislation_notice_ongoing_list import router as notice_list_router
+#from legislation_notice_ongoing_detail import router as notice_detail_router
+
+app = FastAPI()
+
+app.include_router(plenary_router)
+app.include_router(plenary_detail_router)
+app.include_router(notice_list_router)
+#app.include_router(notice_detail_router)
+
+#가상환경 활성화 : .\venv\Scripts\Activate.ps1
+#서버 실행 : uvicorn main:app --reload
+
