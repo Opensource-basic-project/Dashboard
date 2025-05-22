1ca9aac61352ab25ffe46045b2e7b4482e9be236
unknown <cjsgnsvlfcjs@gmail.com>
2025-05-22 16:44:52 +0900

    라우터 추가 (종료된 입법예고 목록)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from plenary_bills_list import router as plenary_router
 from plenary_bills_detail import router as plenary_detail_router
 from legislation_notice_ongoing_list import router as notice_list_router
+from legislation_notice_ended_list import router as notice_list_ended_router
 #from legislation_notice_ongoing_detail import router as notice_detail_router
 
 app = FastAPI()
@@ -10,6 +11,7 @@ app = FastAPI()
 app.include_router(plenary_router)
 app.include_router(plenary_detail_router)
 app.include_router(notice_list_router)
+app.include_router(notice_list_ended_router)
 #app.include_router(notice_detail_router)
 
 #가상환경 활성화 : .\venv\Scripts\Activate.ps1
