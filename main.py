b8b99d475daad08fc75be6bdda9128de23bd1571
unknown <cjsgnsvlfcjs@gmail.com>
2025-05-22 18:01:53 +0900

    라우터 추가

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from plenary_bills_list import router as plenary_router
 from plenary_bills_detail import router as plenary_detail_router
 from legislation_notice_ongoing_list import router as notice_list_router
 from legislation_notice_ended_list import router as notice_list_ended_router
-#from legislation_notice_ongoing_detail import router as notice_detail_router
+from legislation_notice_ongoing_detail import router as notice_detail_router
+from legislation_notice_ended_detail import router as notice_detail_ended_router
 
 app = FastAPI()
 
@@ -12,7 +13,8 @@ app.include_router(plenary_router)
 app.include_router(plenary_detail_router)
 app.include_router(notice_list_router)
 app.include_router(notice_list_ended_router)
-#app.include_router(notice_detail_router)
+app.include_router(notice_detail_router)
+app.include_router(notice_detail_ended_router)
 
 #가상환경 활성화 : .\venv\Scripts\Activate.ps1
 #서버 실행 : uvicorn main:app --reload
