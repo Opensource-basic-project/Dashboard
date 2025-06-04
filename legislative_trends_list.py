e6aae2f63d794eb79294232283d6766b535d5c72
cheonhunpil <cjsgnsvlfcjs@chungbuk.ac.kr>
2025-06-04 12:52:09 +0900

    검색 기능 수정

--- legislative_trends_list.py
+++ legislative_trends_list.py
@@ -3,6 +3,7 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from db import SessionLocal, ForeignLawTrend
 import math
+from sqlalchemy import or_, not_
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -14,6 +15,14 @@ def get_db():
     finally:
         db.close()
 
+STANDARD_NATIONS = ['미국', '일본', '독일', 'EU', '영국', '프랑스']
+STANDARD_COMMITTEES = [
+    '국회운영위원회', '법제사법위원회', '정무위원회', '기획재정위원회', '교육위원회',
+    '과학기술정보방송통신위원회', '외교통일위원회', '국방위원회', '행정안전위원회', '문화체육관광위원회',
+    '농림축산식품해양수산위원회', '산업통상자원중소벤처기업위원회', '보건복지위원회',
+    '환경노동위원회', '국토교통위원회', '정보위원회', '여성가족위원회', '예산결산특별위원회'
+]
+
 @router.get("/legislative-trends")
 def legislative_trends(
     request: Request,
@@ -21,10 +30,12 @@ def legislative_trends(
     size: int = 15,
     keyword: str = "",
     nation: str = "",
+    committee: str = "",
     db: Session = Depends(get_db)
 ):
     query_obj = db.query(ForeignLawTrend)
 
+    # 키워드 검색
     if keyword:
         like_keyword = f"%{keyword}%"
         query_obj = query_obj.filter(
@@ -32,8 +43,23 @@ def legislative_trends(
             ForeignLawTrend.asc_info.ilike(like_keyword)
         )
     
-    if nation:
+    # 국가 필터
+    if nation == "기타":
+        query_obj = query_obj.filter(~ForeignLawTrend.nation_name.in_(STANDARD_NATIONS))
+    elif nation:
         query_obj = query_obj.filter(ForeignLawTrend.nation_name == nation)
+
+    # 상임위원회 필터
+    if committee == "기타":
+        query_obj = query_obj.filter(
+            or_(
+                not_(ForeignLawTrend.asc_info.in_(STANDARD_COMMITTEES)),
+                ForeignLawTrend.asc_info == None,
+                ForeignLawTrend.asc_info == ""
+            )
+        )
+    elif committee:
+        query_obj = query_obj.filter(ForeignLawTrend.asc_info == committee)
 
     total_count = query_obj.count()
     rows = query_obj.order_by(ForeignLawTrend.procl_date.desc()) \
@@ -61,6 +87,7 @@ def legislative_trends(
         "size": size,
         "keyword": keyword,
         "nation": nation,
+        "committee": committee,
         "total_count": total_count,
         "start_page": start_page,
         "end_page": end_page,
--- plenary_bills_list.py
+++ plenary_bills_list.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from db import SessionLocal, PlenaryBill  # PlenaryBill 테이블 import
 import math
 from sqlalchemy import not_
+from sqlalchemy import or_, and_
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -34,7 +35,6 @@ def get_plenary_bills(
             (PlenaryBill.proposer.ilike(query_filter))
         )
 
-
     STANDARD_COMMITTEES = [
         '국회운영위원회', '법제사법위원회', '정무위원회', '기획재정위원회', '교육위원회',
         '과학기술정보방송통신위원회', '외교통일위원회', '국방위원회', '행정안전위원회', '문화체육관광위원회',
@@ -42,9 +42,33 @@ def get_plenary_bills(
         '환경노동위원회', '국토교통위원회', '정보위원회', '여성가족위원회', '예산결산특별위원회'
     ]
     if committee == "기타":
-        query_obj = query_obj.filter(not_(PlenaryBill.committee_nm.in_(STANDARD_COMMITTEES)))
+        query_obj = query_obj.filter(
+            or_(
+                not_(PlenaryBill.committee_nm.in_(STANDARD_COMMITTEES)),
+                PlenaryBill.committee_nm == None,
+                PlenaryBill.committee_nm == ""
+            )
+        )
     elif committee:
         query_obj = query_obj.filter(PlenaryBill.committee_nm == committee)
+        
+    if result:
+        if result == "원안가결":
+            query_obj = query_obj.filter(PlenaryBill.proc_result_cd == "원안가결")
+        elif result == "수정가결":
+            query_obj = query_obj.filter(
+                PlenaryBill.proc_result_cd.in_(["수정가결", "대안반영가결"])
+            )
+        elif result == "철회":
+            query_obj = query_obj.filter(PlenaryBill.proc_result_cd == "철회")
+        elif result == "부결":
+            query_obj = query_obj.filter(
+                PlenaryBill.proc_result_cd.in_(["부결"])
+            )
+        elif result == "기타":
+            excluded = ["원안가결", "수정가결", "대안반영가결", "부결", "철회"]
+            query_obj = query_obj.filter(~PlenaryBill.proc_result_cd.in_(excluded))
+
         
     
     total_count = query_obj.count()
@@ -88,4 +112,5 @@ def get_plenary_bills(
         "start_page": start_page,
         "end_page": end_page,
         "total_pages": total_pages, 
+        "result": result,
     })
